d70db4d3844ca47299afed64e216089b930017ba
Miguel Isabel <miguelis@ucm.es>
2016-05-06 20:43:40 +0200

    Receiver working

--- Communication/prueba_receiver/prueba_receiver.ino
+++ Communication/prueba_receiver/prueba_receiver.ino
@@ -1,17 +1,32 @@
 #include "Comm.h"
 SoftwareSerial serial1(10,11);
-Comm comm;
-
+//Comm comm;
+char str[200];
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  while (!serial1) {;}
+  while(!serial1){;}
   serial1.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Data data = comm.receive(serial1);
-  Serial.print(data.type);
-  Serial.println(data.value);
+  int i = 0;
+  char f;
+  if(serial1.available()){
+    str[i] = serial1.read(); Serial.print(str[0]);
+    delay(100);
+    while(serial1.available() > 0 && i < 198){
+      if (isalpha(serial1.peek())){ break;}
+      f = serial1.read();
+      Serial.print(f);
+    }
+    str[i++] = '\0';
+    if(i>0){
+      Serial.println();
+    }
+  }
+  //Data data = comm.receive();
+  //Serial.print(data.type);
+  //Serial.println(data.value);
 }
