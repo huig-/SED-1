c7fd7ed17adc1c196fcb6cf72d19720595bcfea7
tutugordillo <tutugordillo@gmail.com>
2016-05-17 16:46:55 +0200

    Proyecto LCD funcionando

---
+++ LCD/Debug/sources.mk
@@ -0,0 +1,50 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ELF_SRCS := 
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+CC_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+CPP_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+CC_DEPS := 
+C++_DEPS := 
+OBJS := 
+C_UPPER_DEPS := 
+CXX_DEPS := 
+SECONDARY_FLASH := 
+SECONDARY_SIZE := 
+ASM_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+CPP_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src/Utilities/Fonts \
+src/Utilities/Log \
+src \
+system/include/BSP/Components/cs43l22 \
+system/include/BSP/Components/ili9325 \
+system/include/BSP/Components/ili9341 \
+system/include/BSP/Components/l3gd20 \
+system/include/BSP/Components/lis302dl \
+system/include/BSP/Components/lis3dsh \
+system/include/BSP/Components/lsm303dlhc \
+system/include/BSP/Components/ov2640 \
+system/include/BSP/Components/stmpe1600 \
+system/include/BSP/Components/stmpe811 \
+system/include/BSP/Components/ts3510 \
+system/include/BSP/Components/wm8994 \
+system/src/cmsis \
+system/src/cortexm \
+system/src/diag \
+system/src/newlib \
+system/src/stm32f4-hal \
+
