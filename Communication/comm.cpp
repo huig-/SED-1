4ded746816db716c6c29517bde210936ba6810e8
huig- <igago@ucm.es>
2016-05-26 13:25:40 +0200

    Comm

--- Communication/comm.cpp
+++ Communication/comm.cpp
@@ -4,19 +4,31 @@ void Comm::send(SoftwareSerial serial, float data, data_type type) {
     char header;
     switch (type) {
 	case LIGHT:
+	    char d;
 	    header = pLIGHT;
+	    if (data < 0.02 && data > 0.001) d = NIGHT;
+	    else if (data >= 0.02 && data < 0.3) d = MOON;
+	    else if (data >= 5 && data < 50) d = CLOUDY_INDOOR;
+	    else if (data >= 50 && data < 500) d = CLOUDY_OUTDOOR;
+	    else if (data >= 100 && data < 1000) d = SUNNY_INDOOR;
+	    else if (data >= 10000) d = SUNLIGHT;
+	    serial.print(header);
+	    serial.print(c);
+	    serial.print('\n');
 	    break;
 	case TEMP:
 	    header = pTEMP;
+	    serial.print(header);
+	    serial.print(data);
+	    serial.print('\n');
 	    break;
 	case HUM:
 	    header = pHUM;
+	    serial.print(header);
+	    serial.print(data);
+	    serial.print('\n');
 	    break;
     }
-    serial.print(header);
-    serial.print(data);
-    serial.print('\n');
-    
 }
 
 Data Comm::receive(SoftwareSerial serial) {
--- Communication/comm.h
+++ Communication/comm.h
@@ -12,6 +12,14 @@
 #define pHUM 'h'
 #define SIZE 50
 
+#define NIGHT '0'
+#define MOON '1'
+#define CLOUDY_INDOOR '2'
+#define CLOUDY_OUTDOOR '3'
+#define SUNNY_INDOOR '4'
+#define SUNLIGHT '5'
+#define ART_LIGHT '6'
+
 typedef enum { LIGHT, TEMP, HUM} data_type;
 struct Data {
     data_type type;
